cf40bad4110b8b747a0adc0b210519cdecdef3a4
eddybruv <edwinajong@gmail.com>
2021-07-22 15:47:14 +0100

    takes radius from user

---
+++ chapter_2/exercise_3.c
@@ -0,0 +1,16 @@
+/*Date: 22-07-2021
+    by eddybruv
+    */
+
+#include <stdio.h>
+
+#define FRACTION 4.0f / 3.0f
+#define PI 3.14f
+
+int main(void){
+    int volume, radius;
+    printf("Input the radius of the sphere: ");
+    scanf("%d", &radius);
+    volume = FRACTION * PI *(radius * radius * radius);
+    printf("The volume of the 10m raadius sphere is %dm", volume);
+    return 0;
