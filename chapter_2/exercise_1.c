db80697d6ed34820b54ca480055a1fd2993202ae
eddybruv <edwinajong@gmail.com>
2021-07-22 15:38:16 +0100

    code to print a tick using astericks

---
+++ chapter_2/exercise_1.c
@@ -0,0 +1,17 @@
+/*Date: 22-07-2021
+    by eddybruv
+    */
+
+
+#include <stdio.h>
+
+int main(void){
+    printf("       *\n");
+    printf("      *\n");
+    printf("     *\n");
+    printf("*   *\n");
+    printf(" * *\n");
+    printf("  *\n");
+
+    return 0;
+}
