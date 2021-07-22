6c1937371101b000bf2fcb4c2a274e13d8a021fc
eddybruv <edwinajong@gmail.com>
2021-07-22 04:18:08 +0100

    computes dimensional weight

---
+++ chapter_2/dweight.c
@@ -0,0 +1,22 @@
+/*Date : 22-02-2021
+    by eddybruv
+    */
+
+//Computes the dimensional weight of a 12 x 10 x 8 box
+
+#include <stdio.h>
+
+int main(void){
+    int height, length, width, volume, weight;
+
+    height = 8;
+    length = 12;
+    width = 10;
+    volume = (volume + 165)  / 166;
+
+    printf("Dimensions: %dx%dx%d\n", length, width, height);
+    printf("Voulume (cubic inches): %d\n", volume);
+    printf("Dimensional weight (pounds): %d \n", weight);
+
+    return 0;
+}
