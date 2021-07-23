8bb14038746a8ea18c2de3a4495e381853d93b51
eddybruv <edwinajong@gmail.com>
2021-07-23 04:58:18 +0100

    tabs....

---
+++ chapter_3/exercise_1.c
@@ -0,0 +1,13 @@
+/*Date: 223-07-2021
+    by eddybruv
+    */
+
+#include <stdio.h>
+
+int main(void){
+    printf("Item\tUnit\tPurchase\n");
+    printf("    \tPrice\tDate\n");
+    printf("583 \t$ 13.50\t10/24/2010\n");
+
+    return 0;
+}
