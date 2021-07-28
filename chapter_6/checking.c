566a2050873b3681f8cd54a6d71eb1ec1fcc452b
eddybruv <edwinajong@gmail.com>
2021-07-28 04:29:52 +0100

    Balances a checkbook using loops

---
+++ chapter_6/checking.c
@@ -0,0 +1,40 @@
+/*Date: 28-07-2021
+    by eddybruv
+    */
+
+#include <stdio.h>
+
+int main(void){
+    int cmd;
+    float balance = 0.0f, credit, debit;
+
+    printf("*** ACME checkbook-balancing program***\n");
+    printf("Commands: 0=clear, 1=credit, 2=debit 3=balance, 4=exit\n");
+    for(;;){
+        printf("Enter command: ");
+        scanf("%d", &cmd);
+        switch(cmd){
+            case 0:
+                balance = 0.0f;
+                break;
+            case 1:
+                printf("Enter amount of credit: ");
+                scanf("%f", &credit);
+                balance += credit;
+                break;
+            case 2:
+                printf("Enter amount of debit: ");
+                scanf("%f", &debit);
+                balance -= debit;
+                break;
+            case 3: 
+                printf("Current balance: $%.2f\n", balance);
+                break;
+            case 4: 
+                return 0;
+            default:
+                printf("Commands: 0=clear, 1=credit, 2=debit 3=balance, 4=exit\n");
+                break;
+        }
+    }
+}
