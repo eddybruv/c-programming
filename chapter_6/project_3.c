007e852dfb7c62b70994aa52b495c12f8004533c
eddybruv <edwinajong@gmail.com>
2021-07-29 18:25:52 +0100

    finding the simplest form of any fraction

---
+++ chapter_6/project_3.c
@@ -0,0 +1,22 @@
+/*Date: 29-07-2021
+    by eddybruv
+    */
+
+#include <stdio.h>
+
+int main(void){
+    int m, n, r, gcd;
+    printf("Enter a fraction: ");
+    scanf("%d/%d", &m, &n);
+
+    int d = n;
+    while(n > 0){
+        r = m % n;
+        m = n;
+        n = r;
+    }
+
+    printf("In lower terms: %d/%d", (m/m), (d/m));
+    return 0;
+
+}
