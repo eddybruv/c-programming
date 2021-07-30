8126463c66b871eb60d5403d8f68e82b4b598a9b
eddybruv <edwinajong@gmail.com>
2021-07-30 02:41:40 +0100

    prints out days in a month in calendar format

---
+++ chapter_6/project_8.c
@@ -0,0 +1,31 @@
+/*Date: 29-07-2021
+    by eddybruv
+    */
+
+#include <stdio.h>
+
+int main(void){
+    int days, day;
+
+    printf("Enter number of days in a month: ");
+    scanf("%d", &days);
+    printf("Enter starting day of the week (1=Sun, 7=Sat): ");
+    scanf("%d", &day);
+
+    //number of iterations needed
+    int n = days + (day - 1);
+    
+    int day_of_the_week = 1;
+
+    for(int i = 1;i <= n;i++){
+        if (i < day){
+            printf("  ");
+        }
+        else
+            printf("%3d", day_of_the_week++);
+        if(i % 7 == 0)
+            printf("\n");    
+    }
+
+    return 0;
+}
