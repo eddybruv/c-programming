25f53cc4a2caa7abf9a51401b13220772bfb16ff
eddybruv <edwinajong@gmail.com>
2021-07-31 03:39:59 +0100

    Determine the length of a message

---
+++ chapter_7.c/length_2.c
@@ -0,0 +1,17 @@
+/*Date: 31-07-2021
+    by eddybruv
+    */
+
+#include <stdio.h>
+
+int main(void){
+    int len = 0;
+
+    printf("Enter a message: ");
+    
+    while(getchar() != '\n')
+        len++;
+
+    printf("Your message was %d characters long.\n", len);
+    return 0;
+}
