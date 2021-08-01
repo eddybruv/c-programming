464396efc5679b06555fb0578c9509a5df99ebc4
eddybruv <edwinajong@gmail.com>
2021-08-01 04:23:13 +0100

    modified square2.c to print sth after every 24 sqrs

---
+++ chapter_7.c/project_2.c
@@ -0,0 +1,25 @@
+/*Date: 01-08-2021
+    by eddybruv
+    */
+
+#include <stdio.h>
+
+int main(void){
+        int i, n;
+
+    printf("This program printf a table of squares.\n");
+    printf("Enter number of entries in table: ");
+    scanf("%d", &n);
+
+    for(i = 1; i <= n; i++){
+        printf("%10d%10d\n", i, i*i);
+        while(i % 24 == 0){
+            printf("\nPress Enter to continue...\n");
+            getchar();
+            break;
+        }
+    }
+
+                
+    return 0;
+}
