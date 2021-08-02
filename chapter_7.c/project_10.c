bd3efa31fb88dc5beef5cb195dfd291a1c32e399
eddybruv <edwinajong@gmail.com>
2021-08-02 19:55:33 +0100

    vowel counter

---
+++ chapter_7.c/project_10.c
@@ -0,0 +1,16 @@
+/*Date: 01-08-2021
+    by eddybruv
+    */
+
+#include <stdio.h>
+
+int main(void){
+    char word;
+    int vowel = 0;
+    printf("Enter a sentence: ");
+    while((word = getchar()) != '\n'){
+        if(word == 'a' || word == 'e' || word == 'o' || word == 'u' || word == 'i')
+        vowel += 1;
+    }
+    printf("The number of vowels in the sentence is %d", vowel);
+}
