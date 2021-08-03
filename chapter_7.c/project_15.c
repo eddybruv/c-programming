1a1114ecc71afed70cbf068aa265730620704736
eddybruv <edwinajong@gmail.com>
2021-08-04 00:02:27 +0100

    calculates factorial

--- chapter_7.c/project_15.c
+++ chapter_7.c/project_15.c
@@ -6,11 +6,16 @@
 
 int main(void){
     int num, a, count = 0;
-    long long factorial = 1;
+    long double factorial = 1;
     /*short: highest value for num is 7
      *int: highest value for num is 12
      *long: highest value for num is 16
      *long long: highest value for num is 20
+     *float: heighest value for num is 34 and thats bcuz 35 upward
+             gives infinity
+     *double: heighest value for num is 170 and thats bcuz 170 upward
+             gives infinity 
+     *long double: I got tired searching :)       
      */
     printf("Enter a positive interger: ");
     scanf("%d", &num);
@@ -22,6 +27,6 @@ int main(void){
         if(factorial <= 0)
             printf("This is wrong %d\n", count++);
     } 
-    printf("Facttorial of %lld: %lld\n", a, factorial);
+    printf("Facttorial of %d: %llf\n", a, factorial);
     return 0;
 }
