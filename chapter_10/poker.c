bdeebe2ee9f0f2e33637901c3c91cc4bad85d933
eddybruv <edwinajong@gmail.com>
2021-08-16 10:41:26 +0100

    poker

--- chapter_10/poker.c
+++ chapter_10/poker.c
@@ -65,19 +65,152 @@ void read_cards(void)
 
         rank_ch = getchar();
         switch (rank_ch){
-            case '0':
+            case '0': 
+                exit(EXIT_SUCCESS); 
             case '2':
+                rank = 0;
+                break;
             case '3':
+                rank = 1;
+                break;
             case '4':
+                rank = 2;
+                break;
             case '5':
+                rank = 3;
+                break;
             case '6':
+                rank = 4;
+                break;
             case '7':
+                rank = 5;
+                break;
             case '8':
+                rank = 6;
+                break;
             case '9':
-            case 't':
-            case 'j':
-            case 'q':
-            case 'a':
+                rank = 7;
+                break;
+            case 't': case 'T':
+                rank = 8;
+                break;
+            case 'j': case 'J':
+                rank = 9;
+                break;
+            case 'q': case 'Q':
+                rank = 10;
+                break;
+            case 'k': case 'K': 
+                rank = 11;
+                break;
+            case 'a': case 'A': 
+                rank = 12;
+                break;
+            default:
+                bad_card = true;
+        }
+
+        suit_ch = getchar();
+        switch(suit_ch){
+            case 'c': case 'C': 
+                suit = 0;
+                break;
+            case 'd': case 'D':
+                suit = 1;
+                break;
+            case 'h': case 'H':
+                suit = 2;
+                break;
+            case 's': case 'S':
+                suit = 3;
+                break;
+            default:
+                bad_card = true;
+        }
+
+        while ((ch = getchar()) != '\n')
+            if(ch != ' ') bad_card = true;
+
+        if(bad_card)
+            printf("Bad card; ignored.\n");
+        else if(card_exists[rank][suit])
+            printf("Duplicate card; ignored.\n");
+        else {
+            num_in_rank[rank]++;
+            num_in_suit[suit]++;
+            card_exists[rank][suit] = true;
+            cards_read++;
         }
     }
+}
+
+/**********************************************************
+ * analyze_hand: Determines whether the hand contains a   *
+ *               straight, a flush, four-of-a-kind,       *
+ *               number of pairs; stores the results into *
+ *               the external variables straight, flush,  *
+ *               four, three, and pairs                   *
+ **********************************************************/
+
+void analyze_hand(void){
+    int num_consec = 0;
+    int rank, suit;
+    straight = false;
+    flush = false;
+    three = false;
+    pairs = 0;
+
+    /* check for flush */
+    for (suit = 0; suit < NUM_SUITS; suit++)
+        if (num_in_suit[suit] == NUM_CARDS)
+            flush = true;
+
+            /* check for straight */
+
+    rank = 0;
+    while (num_in_rank[rank] == 0) rank++;
+    for(; rank < NUM_RANKS && num_in_rank[rank] > 0; rank++)
+        num_consec++;
+    if (num_consec == NUM_CARDS){
+        straight = true;
+        return ;
+    }
+
+    /*chech for 4-of-a-kind, 3-of-a-kind, and pairs */
+    for (rank = 0; rank < NUM_RANKS; rank++){
+        if (num_in_rank[rank] == 4) four = true;
+        if (num_in_rank[rank] == 3) three = true;
+        if (num_in_rank[rank] == 2) pairs++;
+    }
+}
+
+
+/********************************************************
+ * print_result: Prints the classification of the hand, *
+ *               based on the calues of the external    *
+ *               variables straight, flush, four, three,*
+ *               and pairs                              *
+ ********************************************************/
+
+void print_result(void){
+    if (straight && flush ) 
+        printf("Staight flush");
+    else if(four)          
+        printf("Four of a kind");
+    else if(three && pairs == 1)
+        printf("Full house");
+    else if(flush)
+        printf("Flush");
+    else if(straight)
+        printf("Straight");
+    else if(three)
+        printf("Three of a kind");
+    else if(pairs == 2)
+        printf("Two pairs");
+    else if(pairs == 1)
+        printf("Pair");
+    else 
+        printf("High card");
+
+    printf("\n\n");
 }
