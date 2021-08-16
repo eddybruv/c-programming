fd45893f14788e85ec705cda8100fad2b975cfc8
eddybruv <edwinajong@gmail.com>
2021-08-16 21:52:03 +0100

    Prints a bad pun

---
+++ chapter_9/pun.c
@@ -0,0 +1,14 @@
+/*Date: 16-08-2021
+    by eddybruv
+    */
+
+#include <stdio.h>
+
+void print_pun(void){
+    printf("To C, or not to C: that's the question.\n");
+}
+
+int main(void){
+    print_pun();
+    return 0;
+}
