60b04d4ffe12a1b0a51fc2381b33fc90a473a4c5
eddybruv <edwinajong@gmail.com>
2021-08-20 20:37:22 +0100

    function to find largest number in an array

---
+++ chapter_12/find_largest.c
@@ -0,0 +1,20 @@
+/*Date: 20-08-2021
+    by eddybruv
+    */
+
+int find_largest(int a[], int n)
+{
+    int i, max;
+
+    max = a[0];
+    for (i = 1; i < n; i++)
+        if (a[i] > max)
+            max = a[i];
+
+    return max;
+}
+
+/**
+ * Suppose that we call find_largest as follows:
+ * largest = find_largest(b, N);
+ * /
