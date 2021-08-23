3925e271f45ca58af468f1209d3d87e5e61a84bb
eddybruv <edwinajong@gmail.com>
2021-08-23 12:34:08 +0100

    solution to exercise 3

---
+++ chapter_12/exercises/03.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+
+#define N 10
+
+int main(void){
+    int a[N] = {1, 2, 3, 4, 5, 6, 7, 8, 9, 10};
+    int *p = &a[0], *q = &a[N-1], temp;
+
+    while (p < q){
+        temp = *p; 
+        *p++ = *q;
+        *q-- = temp;
+    }
+    for(int i = 0; i <= 10; i++)
+        printf("%d ", a[i]);
+
+
+    //prints out the elements in reverse order
+    
+    return 0;
+}
