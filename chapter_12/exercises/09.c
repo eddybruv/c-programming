3f5bf8ec6a735012f5011089b7caa72915afa405
eddybruv <edwinajong@gmail.com>
2021-08-24 05:05:13 +0100

    modified exercise 9

--- chapter_12/exercises/09.c
+++ chapter_12/exercises/09.c
@@ -1,6 +1,19 @@
+#include <assert.h>
+
 double inner_product(const double *a, const double *b, int n)
 {
-   int sum = 0, *p, *q;
-   for(p = a, q = b, p < a + n; p++)
-    sum += (*p++ * *q++);
+   double sum = 0;
+   for(n; n > 0; n--)
+        sum += (*a++ * *b++);
+
+    return sum;
+}
+
+int main(void){
+    double a[] = {6, 7, 8 ,9, 10};
+    double b[] = {1, 2, 3, 4, 5};
+
+    double sum = inner_product(a, b, 5);
+    assert(sum == 130);
+    return 0;
 }
