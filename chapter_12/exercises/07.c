9a13d5ac4809ad9451bdb2aad15fb7d9138c0a0f
eddybruv <edwinajong@gmail.com>
2021-08-24 05:26:08 +0100

    modified exercise 7

--- chapter_12/exercises/07.c
+++ chapter_12/exercises/07.c
@@ -1,14 +1,21 @@
 #include <stdbool.h>
+#include <assert.h>
 
 bool search(const int a[], int n, int key)
 {
-    int *p;
-    p = a;
-    for(p = a; p < a + n; p++)
+    for(n; n > 0; n--)
     {
-        if(*p == key)
+        if(*a++ == key)
             return true;
         else 
             return false;
     }
+}
+
+int main(void){
+    int arr[] = {1, 2, 3 ,4, 5};
+    assert(search(arr, 5, 1) == true);
+    assert(search(arr, 5, 2) == false);
+
+    return 0;
 }
