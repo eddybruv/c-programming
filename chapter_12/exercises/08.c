16b72007fb9aab2840ab0c8bb64082722006b9c5
eddybruv <edwinajong@gmail.com>
2021-08-24 05:17:32 +0100

    modified exercise 8

--- chapter_12/exercises/08.c
+++ chapter_12/exercises/08.c
@@ -1,7 +1,19 @@
+#include <assert.h>
 
-void store_zeros(int a[], int n)
+void *store_zeros(int a[], int n)
 {
     int *p;
     for(p = a; p < a + n; p++)
         *p++ = 0;
+}
+
+int main(void){
+    int arr[] = {1, 6, 8, 9, 3};
+    store_zeros(arr, 3);
+
+    assert(arr[0] == 0);
+    assert(arr[1] == 0);
+    assert(arr[2] == 0);
+    assert(arr[3] == 0);
+    assert(arr[4] == 0);   
 }
