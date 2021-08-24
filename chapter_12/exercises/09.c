e1fd36c0fb0f21495b559e510f8618e481016592
eddybruv <edwinajong@gmail.com>
2021-08-24 04:14:40 +0100

    minor fixes

--- chapter_12/exercises/09.c
+++ chapter_12/exercises/09.c
@@ -2,5 +2,5 @@ double inner_product(const double *a, const double *b, int n)
 {
    int sum = 0, *p, *q;
    for(p = a, q = b, p < a + n; p++)
-    sum += (*p++ * *q++)
+    sum += (*p++ * *q++);
 }
