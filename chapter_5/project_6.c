6f78246093d7c81ad583311b36f9b63813db1f65
eddybruv <edwinajong@gmail.com>
2021-08-26 05:25:44 +0100

    added project 6

---
+++ chapter_5/project_6.c
@@ -0,0 +1,27 @@
+/*Date: 26-08-2021
+    by eddybruv
+    */
+#include <stdio.h>
+
+int main(void){
+    int d, i1, i2, i3, i4, i5, j1, j2, j3, j4, j5,
+        first_num, second_num, total;
+
+    printf("Enter the first (single) digit: ");
+    scanf("%1d", &d);
+    printf("Enter first group of five digits: ");
+    scanf("%1d%1d%1d%1d%1d", &i1, &i2, &i3, &i4, &i5);
+    printf("Enter second group of five digits: ");
+    scanf("%1d%1d%1d%1d%1d", &j1, &j2, &j3, &j4, &j5);
+    
+    first_num = d + i2 + i4 + j1 + j3 +j5;
+    second_num = i1 + i3 + i5 + j2 + j4;
+    total = 3 * first_num + second_num;
+
+    int check = 9 - ((total - 1) % 10);
+
+    if (check > 0 && check <10)
+        printf("VALID");
+    else printf("INVALID");
+    return 0;
+}
