100b28f4b7bba04a45a213d69b3833c6e1dd0728
eddybruv <edwinajong@gmail.com>
2021-08-29 20:37:12 +0100

    added project 14

---
+++ chapter_8/project_14.c
@@ -0,0 +1,44 @@
+/*Date: 14-08-2021
+    by eddybruv
+    */
+
+#include <stdio.h>
+#include <string.h>
+
+#define SIZE 80
+
+int main(void){
+    char input, sentence[SIZE + 1], terminal = 0;
+    int last_space = SIZE + 1, i = 0,j = 0;
+
+    printf("Enter a sentence: ");
+    for(int i = 0; (input = getchar()) != '\n'; i++){
+        if(input == '.' || input == '?' || input == '!'){
+            last_space = i;
+            terminal = input;
+            break;
+        }
+        sentence[i] = input;
+    }
+
+    if(terminal == 0){
+        printf("No terminating character detected");
+        return 1;
+    }
+
+    printf("Reversal of sentence: ");
+    for(i = last_space; i > 0; i--){
+        if(sentence[i] == ' '){
+            for (j = i + 1; j != last_space;j++){
+                putchar(sentence[j]);
+            }
+            last_space = i;
+            putchar(sentence[i]);
+        }
+    }
+    while (sentence [i] != '\0' && sentence[i] != ' '){
+        putchar(sentence[i++]);
+    }
+    printf("%c\n", terminal);
+    return 0;
+}
