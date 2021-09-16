1f22d0e3facb6df4c9307750806496ac18f8220d
eddybruv <edwinajong@gmail.com>
2021-09-16 17:43:12 +0100

    added exercice 1c

---
+++ Chapter_14/Exercises/01c.c
@@ -0,0 +1,15 @@
+/**
+ * Date:16-09-2021
+ * by eddybruv
+ **/
+
+#include <stdio.h>
+
+#define PRO(x, y) (((x) * (y) > 100)? 1 : 0)
+
+void main(){
+    int x, y;
+    printf("Enter two numbers: ");
+    scanf("%d, %d", &x, &y);
+    printf("The value is %d", PRO(x, y));
+}
