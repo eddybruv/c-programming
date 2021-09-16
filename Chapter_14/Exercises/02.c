8b5680fc51898af0dd1f5180b4dfdd3af5380cce
eddybruv <edwinajong@gmail.com>
2021-09-16 17:50:50 +0100

    added project 2

---
+++ Chapter_14/Exercises/02.c
@@ -0,0 +1,14 @@
+/**
+ * Date:16-09-2021
+ * by eddybruv
+ **/
+
+#include <stdio.h>
+
+#define NELEMS(a) (sizeof(a) / sizeof(a[0]))
+
+int main(void){
+    int a[] = {3, 5, 6, 7, 2 ,1 , 5, 9};
+    printf("Number of elements: %d", NELEMS(a));
+    return 0;
+}
