4da042b50c025a6e09e79460446f0db04531f979
eddybruv <edwinajong@gmail.com>
2021-09-16 17:56:48 +0100

    added project 3b

---
+++ Chapter_14/Exercises/03b.c
@@ -0,0 +1,14 @@
+/**
+ * Date:16-09-2021
+ * by eddybruv
+ **/
+
+#include <stdio.h>
+
+#define DOUBLE(x) (2 * (x))
+
+int main(void){
+    printf("The value of DOUBLE(1 + 2): %d\n", DOUBLE(1 + 2));
+    printf("The value of 4 / DOUBLE(2): %d\n", (4/DOUBLE(1 + 2)));
+    return 0;
+}
