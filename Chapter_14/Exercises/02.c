bc2b623323f78bb91999b731fbadb3ad7476f165
eddybruv <edwinajong@gmail.com>
2021-09-20 20:05:57 +0100

    added project 2

--- Chapter_14/Exercises/02.c
+++ Chapter_14/Exercises/02.c
@@ -8,6 +8,8 @@
 #define NELEMS(a) (sizeof(a) / sizeof(a[0]))
 
 int main(void){
+    //can be changed to an array of characters
+    //results are different
     int a[] = {3, 5, 6, 7, 2 ,1 , 5, 9};
     printf("Number of elements: %d", NELEMS(a));
     return 0;
