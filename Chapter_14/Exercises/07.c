6869f96b2fd9208ebdfbc1fe7dab7247e90d8f8e
eddybruv <edwinajong@gmail.com>
2021-09-20 20:55:53 +0100

    added exercise 7 and added more infomation

--- Chapter_14/Exercises/07.c
+++ Chapter_14/Exercises/07.c
@@ -11,11 +11,24 @@ type type##_max(type x, type y)   \
     return x > y ? x : y;         \
 }
 
+typedef unsigned long unsigned_long;
+
 int main(void){
-    GENERIC_MAX(long)
+    GENERIC_MAX(unsigned_long)
 
     /**
+     * GENERIC_MAX(long)
      * Expands to:
      * long long_max(long x, long y) { return x > y ? x : y; }
      **/
+
+    /**
+     * GENERIC_MAX(unsigned long)
+     * Expands to:
+     * unsigned long unsigned long_max(unsigned long x, unsigned long y) { return x > y ? x : y; }
+     * 
+     * This is faulty because it has a space bar in between the "type name"
+     * 
+     * We can correct it using the "typedef" keyword and redefine the basic type
+     **/ 
 }
