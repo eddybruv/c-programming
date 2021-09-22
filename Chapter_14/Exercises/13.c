9a0642bf6d5b9b4a5f612e88cdc489d836223877
eddybruv <edwinajong@gmail.com>
2021-09-22 10:51:17 +0100

    added exercise 13

---
+++ Chapter_14/Exercises/13.c
@@ -0,0 +1,30 @@
+/**
+ * Date: 22-09-2021
+ * by eddybruv
+ **/
+
+#include <stdio.h>
+
+#define N 100
+
+void f(void);
+
+int main(void)
+{
+    f();
+#ifdef N
+#undef N
+#endif
+    return 0;
+}
+
+void f(void)
+{
+#if defined (N)
+    printf("N is %d\n", N);
+#else
+    printf("N is undefined\n");
+#endif
+}
+
+//Output: N is undefined.
