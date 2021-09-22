e8c1efc09e13888ab250c8904d249d89636ecc63
eddybruv <edwinajong@gmail.com>
2021-09-22 10:04:55 +0100

    added exercise 12

---
+++ Chapter_14/Exercises/12.c
@@ -0,0 +1,16 @@
+/**
+ * Date: 22-09-2021
+ * by eddybruv
+ **/
+
+#include <stdio.h>
+
+#define M 10
+
+/**
+ * #if M               (true)
+ * #ifdef M            (true)
+ * #ifndef M           (false)
+ * #if defined(M)      (true)
+ * #if !defined(M)     (false)
+ **/
