0cc40cb55af45ba803aa399032c5529a1d6cf816
eddybruv <edwinajong@gmail.com>
2021-09-27 17:43:07 +0100

    contains main functions

--- Chapter_15/Text Formatting/justify.c
+++ Chapter_15/Text Formatting/justify.c
@@ -23,14 +23,14 @@ int main(void)
         read_word(word, MAX_WORD_LEN+1);
         word_len = strlen(word);
         if (word_len == 0){
-            flush_line;
+            flush_line();
             return 0;
         }
-
+        
         if (word_len > MAX_WORD_LEN)
             word[MAX_WORD_LEN] = '*';
-            
-        if (word_len +1 > space_remaining()){
+
+        if (word_len + 1 > space_remaining()){
             write_line();
             clear_line();
         }
