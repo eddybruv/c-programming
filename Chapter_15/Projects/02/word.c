20e44456b0300b0c84b3b4a5b1f3ddb651d89b03
eddybruv <edwinajong@gmail.com>
2021-09-30 19:45:34 +0100

    done with project 2

---
+++ Chapter_15/Projects/02/word.c
@@ -0,0 +1,31 @@
+#include <stdio.h>
+#include "word.h"
+
+int read_char(void)
+{
+    int ch = getchar();
+
+    if (ch == '\n' || ch == '\t')
+        return ' ';
+    return ch;
+}
+
+int read_word(char *word, int len)
+{
+    int ch, pos = 0;
+
+    while((ch = read_char()) == ' ')
+        ;
+        
+    while (ch != ' ' && ch != EOF){
+        if (pos < len)
+            word[pos++] = ch;
+        ch = read_char();
+    }
+    word[pos] = '\0';
+
+    if(pos > len - 1)
+        word[len - 1] = "*";
+    
+    return pos;
+}
