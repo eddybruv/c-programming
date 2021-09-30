f608899dc3ca2e5a3c57818b96aff2379d3ea3cd
eddybruv <edwinajong@gmail.com>
2021-09-30 20:42:57 +0100

    added project 4

---
+++ Chapter_15/Projects/Remind_04/readline.c
@@ -0,0 +1,13 @@
+#include "readline.h"
+#include <stdio.h>
+
+int read_line(char str[], int n){
+    int ch, i = 0;
+
+    while ((ch = getchar()) != '\n')
+        if (i < n)
+            str[i++] = ch;
+
+    str[i] = '\0';
+    return i;
+}
