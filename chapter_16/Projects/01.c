729dcab5d64aea8b3fc876876f89ac8998294b35
eddybruv <edwinajong@gmail.com>
2021-10-17 10:53:40 +0100

    country code project

--- chapter_16/Projects/01.c
+++ chapter_16/Projects/01.c
@@ -15,7 +15,7 @@ struct dial_codes {
 
 
 int main(void){
-    const struct dial_codes country_code = {
+    const struct dial_codes country_code[] = {
         {"Argentina",    54}, {"Bangladesh",     880}, 
         {"Brazil",       55}, {"Burma",           95},
         {"China",        86}, {"Colombia",        57},
@@ -24,7 +24,7 @@ int main(void){
         {"Germany",      49}, {"India",           91},
         {"Indonesia",    62}, {"Iran",            98},
         {"Italy",        39}, {"Japan",           81},
-        {"Mexico",       52}, {"Nigeria",        243},
+        {"Mexico",       52}, {"Nigeria",        247},
         {"Pakistan",     92}, {"Philippenes",     63},
         {"Poland",       48}, {"Russia",           7},
         {"South Africa", 27}, {"South Korea",     82},
@@ -34,12 +34,19 @@ int main(void){
         {"United States", 1}, {"vietnam",         84}
     };
 
-    int your_code;
+    int your_code, check = 0;
     printf("Enter your code: ");
     scanf("%d", &your_code);
 
     for(int i = 0; i < LENGTH(country_code);i++){
-        if(your_code == country_code[i].code)
-            printf("Country is: %s", country_code[i].country)
+        if(your_code == country_code[i].code){
+            printf("Country is: %s", country_code[i].country);
+            check = 1;
+        }
     }
+
+    if(check != 1)
+        printf("Invalid Code.");
+
+    return 0;
 }
