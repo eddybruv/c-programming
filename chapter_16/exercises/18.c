2f068c1ab738624587b910cb3add41c06b3d0ad8
eddybruv <edwinajong@gmail.com>
2021-10-26 01:20:58 +0100

    chessboard

---
+++ chapter_16/exercises/18.c
@@ -0,0 +1,108 @@
+#include <stdio.h>
+
+#define BOARD_LEN 8
+
+tyepdef enum {
+    EMPTY, 
+    PAWN, 
+    BISHOP, 
+    KNIGHT, 
+    ROOK, 
+    QUEEN, 
+    KING
+} Piece;
+
+tyepdef enum {
+    BLACK, 
+    WHITE
+} Color;
+
+type struct {
+    Piece piece_type;
+    Color piece_color; 
+} Square;
+
+Square board[BOARD_LEN][BOARD_LEN];
+
+board = {
+    { 
+        (Square) {ROOK, WHITE},
+        (Square) {KNIGHT, WHITE}, 
+        (Square) {BISHOP, WHITE}, 
+        (Square) {QUEEN, WHITE}, 
+        (Square) {KING, WHITE}, 
+        (Square) {BISHOP, WHITE}, 
+        (Square) {KNIGHT, WHITE}, 
+        (Square) {ROOK, WHITE},
+    },
+    {
+        (Square) {PAWN, WHITE},
+        (Square) {PAWN, WHITE},
+        (Square) {PAWN, WHITE},
+        (Square) {PAWN, WHITE},
+        (Square) {PAWN, WHITE},
+        (Square) {PAWN, WHITE},
+        (Square) {PAWN, WHITE},
+        (Square) {PAWN, WHITE},
+    },
+    {
+        (Square) {EMPTY, BLACK},
+        (Square) {EMPTY, BLACK},
+        (Square) {EMPTY, BLACK},
+        (Square) {EMPTY, BLACK},
+        (Square) {EMPTY, BLACK},
+        (Square) {EMPTY, BLACK},
+        (Square) {EMPTY, BLACK},
+        (Square) {EMPTY, BLACK},
+    },
+    {
+        (Square) {EMPTY, BLACK},
+        (Square) {EMPTY, BLACK},
+        (Square) {EMPTY, BLACK},
+        (Square) {EMPTY, BLACK},
+        (Square) {EMPTY, BLACK},
+        (Square) {EMPTY, BLACK},
+        (Square) {EMPTY, BLACK},
+        (Square) {EMPTY, BLACK},
+    },
+    {
+        (Square) {EMPTY, BLACK},
+        (Square) {EMPTY, BLACK},
+        (Square) {EMPTY, BLACK},
+        (Square) {EMPTY, BLACK},
+        (Square) {EMPTY, BLACK},
+        (Square) {EMPTY, BLACK},
+        (Square) {EMPTY, BLACK},
+        (Square) {EMPTY, BLACK},
+    },
+    {
+        (Square) {EMPTY, BLACK},
+        (Square) {EMPTY, BLACK},
+        (Square) {EMPTY, BLACK},
+        (Square) {EMPTY, BLACK},
+        (Square) {EMPTY, BLACK},
+        (Square) {EMPTY, BLACK},
+        (Square) {EMPTY, BLACK},
+        (Square) {EMPTY, BLACK},
+    },
+    {
+        (Square) {PAWN, BLACK},
+        (Square) {PAWN, BLACK},
+        (Square) {PAWN, BLACK},
+        (Square) {PAWN, BLACK},
+        (Square) {PAWN, BLACK},
+        (Square) {PAWN, BLACK},
+        (Square) {PAWN, BLACK},
+        (Square) {PAWN, BLACK},
+    },
+    { 
+        (Square) {ROOK, BLACK},
+        (Square) {KNIGHT, BLACK}, 
+        (Square) {BISHOP, BLACK}, 
+        (Square) {QUEEN, BLACK}, 
+        (Square) {KING, BLACK}, 
+        (Square) {BISHOP, BLACK}, 
+        (Square) {KNIGHT, BLACK}, 
+        (Square) {ROOK, BLACK},
+    },
+}
