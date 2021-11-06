07c080b40560a35fc3f1d9899457a7ab56f46c73
eddybruv <edwinajong@gmail.com>
2021-11-06 14:58:35 +0100

    adding nodes to the list

---
+++ chapter_17/Linked Lists/adding.c
@@ -0,0 +1,89 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+struct node {
+    int value;
+    struct node *next;
+};
+
+void add_end(struct node **point, int n);
+struct node *add_head (struct node *point, int n);
+struct node *add_end1(struct node *list, int n);
+
+int main(void){
+    struct node *head, *point;
+
+    head = malloc(sizeof(struct node));
+    head->value = 1;
+    head->next = NULL;
+
+    head = point;
+
+    for (int i = 0; i < 7;i++){
+        point = add_end1(point, i);
+    }
+
+    for (int i = 0; i < 7;i++){
+        printf("%d -> ", head->value);
+        head = head->next;
+    }
+}
+
+struct node *add_end1(struct node *tracker, int n){
+    struct node *new_node;
+
+    new_node = malloc (sizeof(struct node));
+
+    if(new_node == NULL){
+        printf("Memory not allocated");
+        exit (EXIT_FAILURE);
+    }
+
+    new_node -> value = n;
+    if (tracker == NULL){
+        //point is head
+        tracker = new_node;
+    }
+
+    else {
+        tracker->next = new_node;
+        tracker = tracker->next;
+    }
+
+    tracker->next = NULL;
+
+    return tracker;
+}
+
+void add_end2(struct node **point, int n){
+    struct node *new_node;
+
+    new_node = malloc (sizeof(struct node));
+    if(new_node == NULL){
+        printf("Memory not allocated");
+        exit (EXIT_FAILURE);
+    }
+
+    new_node -> value = n;
+    if (*point == NULL){
+        //point is head
+        (*point) = new_node;
+    }
+
+    else {
+        (*point)->next = new_node;
+        (*point) = (*point) -> next;
+    }
+
+    (*point)->next = NULL;
+}
+
+struct node *add_head(struct node *list, int n){
+    struct node *new_node;
+    new_node = malloc(sizeof(struct node));
+    
+    new_node->value = n;
+    new_node->next = list;
+
+    return new_node;
+}
