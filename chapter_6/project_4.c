372bce57200c5838d2b5c0674d256418228f2d13
eddybruv <edwinajong@gmail.com>
2022-06-06 21:30:31 +0100

    switched to a do while loop

--- chapter_6/project_4.c
+++ chapter_6/project_4.c
@@ -4,24 +4,24 @@
 
 #include <stdio.h>
 
-int main(void){
+int main(void)
+{
     float commission, value = 1;
 
-    
-    while(value > 0){
+    do
+    {
         printf("Enter value of trade: ");
         scanf("%f", &value);
-        if(value == 0)
+        if (value == 0)
         {
-        return 1;
+            return 1;
         }
 
-
-        if(value < 2500.00f)
+        if (value < 2500.00f)
             commission = 30.00f + .017f * value;
         else if (value < 6250.00f)
             commission = 56.00f + .0066f * value;
-        
+
         else if (value < 20000.00f)
             commission = 76.00f + .0034f * value;
 
@@ -30,13 +30,12 @@ int main(void){
 
         else if (value < 500000.00f)
             commission = 155.00f + .0011f * value;
-        
-        else 
+
+        else
             commission = 255.00f + .0009f * value;
 
         if (commission < 39.00f)
             commission = 39.00f;
-            printf("Commission: $%.2f\n", commission);
-    }
-    
+        printf("Commission: $%.2f\n", commission);
+    } while (value > 0);
 }
